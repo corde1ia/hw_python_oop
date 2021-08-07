1366132ce278662315d178d6783d266365f4185c
Natalya <ngkolyadina@1367.ru>
2021-08-07 13:36:02 +0300

    исправленный код по замечаниям  07.08.21

--- homework.py
+++ homework.py
@@ -1,10 +1,16 @@
+"""Код для создания калькулятора
+днегег и калькулятора калорий."""
+
 import datetime as dt
 from typing import Optional
 
 
+DATE_FORMAT = '%d.%m.%Y'
+
+
 class Record:
     """Класс, содержащий в себе описание структуры данных
-    для записей калькуляторов калорий/денег."""
+    для записей калькулятора калорий/денег."""
     amount: float
     comment: str
     date: Optional[str]
@@ -13,7 +19,7 @@ class Record:
         self.amount = amount
         self.comment = comment
         if date is not None:
-            self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
+            self.date = dt.datetime.strptime(date, DATE_FORMAT).date()
         else:
             self.date = dt.date.today()
 
@@ -35,18 +41,17 @@ class Calculator:
     def get_today_stats(self) -> float:
         """"Метод для вывода данных о ккал/деньгах за день."""
         today_stats = 0
-        for r in self.records:
-            if r.date == dt.date.today():
-                today_stats += r.amount
+        date_today = dt.date.today()
+        today_stats = sum(r.amount for r in self.records
+                          if r.date == date_today)
         return today_stats
 
     def get_week_stats(self) -> float:
         """Метод для вывода данных о ккал/деньгах за неделю."""
         week_stats = 0
         week_ago = dt.datetime.now().date() - dt.timedelta(weeks=1)
-        for r in self.records:
-            if r.date > week_ago and r.date <= dt.datetime.now().date():
-                week_stats += r.amount
+        week_stats = sum(r.amount for r in self.records
+                         if dt.datetime.now().date() >= r.date > week_ago)
         return week_stats
 
 
@@ -59,8 +64,7 @@ class CaloriesCalculator(Calculator):
         if super().get_today_stats() < self.limit:
             return (f"Сегодня можно съесть что-нибудь ещё, "
                     f"но с общей калорийностью не более {remainded} кКал")
-        else:
-            return ("Хватит есть!")
+        return "Хватит есть!"
 
 
 class CashCalculator(Calculator):
@@ -72,24 +76,22 @@ class CashCalculator(Calculator):
     def get_today_cash_remained(self, currency) -> str:
         """Метод, вычисляющий остато денежных средств,
         доступных для траты, в заданной валюте."""
-        RATES: dict
         RATES = {
             "usd": (self.USD_RATE, "USD"),
             "eur": (self.EURO_RATE, "Euro"),
             "rub": (self.RUB_RATE, "руб")
         }
 
+        if currency not in RATES:
+            return "Неизвестная валюта"
+
         rate, currency_name = RATES[currency]
         today_stats = super().get_today_stats()
         cash_remained = round((self.limit - today_stats) / rate, 2)
 
-        if currency not in RATES.keys():
-            return ("Неизвестная валюта")
-        else:
-            if cash_remained > 0:
-                return (f"На сегодня осталось {cash_remained} {currency_name}")
-            elif cash_remained == 0:
-                return ("Денег нет, держись")
-            else:
-                return (f"Денег нет, держись: твой долг - "
-                        f"{abs(cash_remained)} {currency_name}")
+        if cash_remained > 0:
+            return (f"На сегодня осталось {cash_remained} {currency_name}")
+        elif cash_remained == 0:
+            return ("Денег нет, держись")
+        return (f"Денег нет, держись: твой долг - "
+                f"{abs(cash_remained)} {currency_name}")
